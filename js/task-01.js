73e74f22c141fa7121130f2f836c0818eaba57ae
BogdanV <zarazailo@gmail.com>
2022-02-09 21:32:00 +0200

    1 task

---
+++ js/task-01.js
@@ -0,0 +1,5 @@
+// const numberOfCategories = document.querySelectorAll("ul").length;
+// console.log(`Number of categories: ${numberOfCategories}`);
+
+const numberOfCategories = document.getElementsByClassName('item').length;
+console.log(`Number of categories: ${numberOfCategories}`);
