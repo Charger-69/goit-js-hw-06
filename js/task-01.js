478cd8f92f5f610c974ce1db5d83ff619d7b630d
BogdanV <zarazailo@gmail.com>
2022-02-10 21:02:38 +0200

    task 1 fin

--- js/task-01.js
+++ js/task-01.js
@@ -3,3 +3,9 @@
 
 const numberOfCategories = document.getElementsByClassName('item').length;
 console.log(`Number of categories: ${numberOfCategories}`);
+
+const сategoryElements = document.querySelectorAll('#categories>li');
+сategoryElements.forEach(element => {
+console.log(`Category: ${element.firstElementChild.textContent}`);
+console.log(`Elements: ${element.lastElementChild.children.length}`)
+})
