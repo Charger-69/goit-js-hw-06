05f3aa7145716fc31a94c685bcb595274f803e71
BogdanV <zarazailo@gmail.com>
2022-02-19 17:55:10 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+const bodyElem = document.body
+const btnElem = document.querySelector('.change-color')
+const spanElem = document.querySelector('.color')
+const bodyBack = bodyElem.style.backgroundColor
+
+btnElem.addEventListener('click', onButtonClick)
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onButtonClick() {
+  const color = getRandomHexColor();
+  bodyElem.style.backgroundColor = color;
+  spanElem.textContent = color;
 }
